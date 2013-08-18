207d9adc26be21202606a2ddfb90ed4324965a56
joueur <yves.tatienne@gmail.com>
2013-08-18 08:22:58 +0000

    Init rep

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Petit::Application.config.secret_key_base = '75b82d281103e1b4f2b86cbb011d654f925f7e0f21b77ccabdba7d961b50e243982ebc37aa3c0ba1099500e73b1bd83a4d174032835d461afd0318da8c41edc7'
